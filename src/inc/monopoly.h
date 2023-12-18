f486bffad0fee9bb86c71e8db0cc6e131cc6c580
Xayiide <wickedcheerio@protonmail.com>
2023-12-19 00:19:39 +0100

    Simulación inicil

---
+++ src/inc/monopoly.h
@@ -0,0 +1,53 @@
+#ifndef MONOPOLY_H_
+#define MONOPOLY_H_
+
+#define MONOPOLY_NUM_CASILLAS 40
+
+char *monopoly_casillas[] = 
+{
+    "Salida",
+    "Propiedad marrón 1",
+    "Caja de comunidad 1",
+    "Propiedad marrón 2",
+    "Impuestos 1",
+    "Estación 1",
+    "Propiedad celeste 1",
+    "Suerte 1",
+    "Propiedad celeste 2",
+    "Propiedad celeste 3",
+
+    "Cárcel",
+    "Propiedad rosa 1",
+    "Servicio público 1",
+    "Propiedad rosa 2",
+    "Propiedad rosa 3",
+    "Estación 2",
+    "Propiedad naranja 1",
+    "Caja de comunidad 2",
+    "Propiedad naranja 2",
+    "Propiedad naranja 3",
+
+    "Parking gratuito",
+    "Propiedad roja 1",
+    "Suerte 2",
+    "Propiedad roja 2",
+    "Propiedad roja 3",
+    "Estación 3",
+    "Propiedad amarilla 1",
+    "Propiedad amarilla 2",
+    "Servicio público 2",
+    "Propiedad amarilla 3",
+
+    "Ir a la cárcel",
+    "Propiedad verde 1",
+    "Propiedad verde 2",
+    "Caja de comunidad 3",
+    "Propiedad verde 3",
+    "Estación 4",
+    "Suerte 3",
+    "Propiedad azul 1",
+    "Impuestos 2",
+    "Propiedad azul 2"
+};
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdlib.h> /* exit, EXIT_FAILURE, strtoul, srand */
+#include <stdint.h> /* uint_t */
+#include <time.h>   /* time   */
+
+#include <unistd.h> /* sleep  */
+
+#include "inc/monopoly.h"
+
+int tirar_dados(void)
+{
+    int value = 0;
+
+    value  = (rand() % 6) + 1;
+    value += (rand() % 6) + 1;
+
+    return value;
+}
+
+int main(int argc, char *argv[])
+{
+    uint64_t num_tiradas = 0;
+    uint64_t lista_caidas[MONOPOLY_NUM_CASILLAS] = { 0 };
+    uint64_t i;
+    int      posicion = 0;
+    int      dados[2];
+    int      tirada_dados;
+    int      dobles_seguidos = 0;
+    //uint64_t ultimo_doble = 0;
+
+    if (argc != 2) {
+        fprintf(stderr, "uso: %s <num tiradas>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    num_tiradas = strtoul(argv[1], NULL, 10);
+
+    srand(time(NULL));
+
+    // Imprimir el array
+    // for (i = 0; i < MONOPOLY_NUM_CASILLAS; i++)
+    //     printf("[%02d] %s\n", i, monopoly_casillas[i]);
+
+    for (i = 0; i < num_tiradas; i++) {
+        dados[0] = (rand() % 6) + 1;
+        dados[1] = (rand() % 6) + 1;
+
+        if (dados[0] == dados[1]) { /* dobles */
+            dobles_seguidos++;           
+        }
+        else {
+            dobles_seguidos = 0;
+        }
+
+
+        if (dobles_seguidos == 3) {
+            posicion = 10; /* Nos vamos a la cárcel por tres dobles seguidos */
+            lista_caidas[posicion]++;
+            //printf("[%ld] Han salido tres dobles %d seguidos. "
+            //        "El último hace %ld tiradas.\n",
+            //        i, dados[0], i - ultimo_doble);
+            //ultimo_doble = i;
+            continue;
+        }
+
+        tirada_dados = dados[0] + dados[1];
+        posicion = (posicion + tirada_dados) % MONOPOLY_NUM_CASILLAS;
+        lista_caidas[posicion]++;
+
+        if (posicion == 30) { /* ir a la cárcel */
+            posicion = 10;
+        }
+    }
+
+    for (i = 0; i < MONOPOLY_NUM_CASILLAS; i++) {
+
+        printf("[%ld = %0.4f%%] %s\n", lista_caidas[i], 
+                                ((lista_caidas[i] * 1.0) / num_tiradas) * 100,
+                                monopoly_casillas[i]);
+    }
+
+    return 0;
+}
